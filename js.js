67487cc9caba17d5e8211a28dd8121f27a2051a1
bimal02 <143879674+bimal02@users.noreply.github.com>
2023-09-10 22:44:10 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,14 @@
+
+    function About(){
+    window.open("about.html") 
+   }
+   function Skills(){
+    window.open("Skills.html")
+    }
+    function cont(){
+        window.open("cont.html")  
+    }
+    function Eduction(){
+        window.open("Eduction.html")  
+    }
+     
